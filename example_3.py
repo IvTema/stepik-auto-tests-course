986009104dac4eee558e0f90be8559683c42ecbe
Nameman <duft.soft@yandex.ru>
2020-06-10 00:15:42 +0300

    Only samples

--- example_3.py
+++ example_3.py
@@ -1,7 +1,7 @@
 import unittest
 from selenium import webdriver
 
-link = "http://suninjuly.github.io/registration1.html"
+link = "http://suninjuly.github.io/registration2.html"
 browser = webdriver.Chrome()
 browser.get(link)
 
