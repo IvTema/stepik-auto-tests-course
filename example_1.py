1b7e7610e48b6bbc0762fa5fe6bd93a06253e18a
Nameman <duft.soft@yandex.ru>
2020-07-18 18:08:13 +0300

    sample

--- example_1.py
+++ example_1.py
@@ -1,39 +1,6 @@
-import pytest
-import math
-import time
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
-def answer():
-    return math.log(int(time.time()))
+# инициализируем драйвер браузера. После этой команды вы должны увидеть новое открытое окно браузера
+driver = webdriver.Firefox()
 
-samples = [
-    ("a", "236895"),
-    ("b", "236896"),
-    ("c", "236897"),
-    ("d", "236898"),
-    ("e", "236899"),
-    ("f", "236903"),
-    ("g", "236904"),
-    ("h", "236905")
-]
-
-@pytest.fixture(scope="function")
-def browser():
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome()
-    yield browser
-    print("\nquit browser..")
-    browser.quit()
-
-@pytest.mark.parametrize('num, code', samples)
-def test_correct_sign(browser, num, code):
-        link = "https://stepik.org/lesson/{}/step/1".format(code)
-        print("Номер ссылки %s" % num)
-        browser.get(link)
-        text_field = WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.TAG_NAME, "textarea")))
-        text_field.send_keys(str(math.log(int(time.time()))))
-        browser.find_element_by_css_selector(".submit-submission").click()
-        print(browser.find_element_by_css_selector('.smart-hints__hint').text)
+driver.get("https://stepik.org/lesson/25969/step/8")
--- example_4.py
+++ example_4.py
@@ -1,37 +1,9 @@
-import unittest
-from selenium import webdriver
-import time
+link = "http://selenium1py.pythonanywhere.com/"
 
-class TestSystem(unittest.TestCase):
-    def test_1st(self):
-        def link_f(link):
-            browser = webdriver.Chrome()
-            browser.get(link)
-            name = browser.find_element_by_css_selector('.first_block>.first_class>.first')
-            name.send_keys('Шингыз')
-            lastname = browser.find_element_by_css_selector('.first_block>.second_class>.second')
-            lastname.send_keys('Возбухамедов')
-            mail = browser.find_element_by_css_selector('.first_block>.third_class>.third')
-            mail.send_keys('itisokto@begay.too')
+def test_guest_should_see_login_link_pass(browser):
+    browser.get(link)
+    browser.find_element_by_css_selector("#login_link")
 
-            # Отправляем заполненную форму
-            button = browser.find_element_by_css_selector("button.btn")
-            button.click()
-
-            # Проверяем, что смогли зарегистрироваться
-            # ждем загрузки страницы
-            time.sleep(1)
-
-            # находим элемент, содержащий текст
-            welcome_text_elt = browser.find_element_by_tag_name("h1")
-            # записываем в переменную welcome_text текст из элемента welcome_text_elt
-            welcome_text = welcome_text_elt.text
-
-            # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
-            self.assertEqual(welcome_text, 'Congratulations! You have successfully registered!',
-                             "button_click_error")
-        link_f("http://suninjuly.github.io/registration1.html")
-        link_f("http://suninjuly.github.io/registration2.html")
-
-if __name__ == "__main__":
-    unittest.main()
+def test_guest_should_see_login_link_fail(browser):
+    browser.get(link)
+    browser.find_element_by_css_selector("#magic_link")
